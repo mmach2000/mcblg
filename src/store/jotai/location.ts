000055393986f1a0941fcb6ed0a9165b088f5552
glatavento <glatavento@outlook.com>
2024-08-21 23:43:21 +0800

    fix: 修复页面跳转

--- src/store/jotai/location.ts
+++ src/store/jotai/location.ts
@@ -2,4 +2,4 @@ import { atomWithLocation } from 'jotai-location';
 
 export const SERIALIZATION_OPTION = { arrayFormat: 'separator' as const, arrayFormatSeparator: '|' as const };
 
-export const locationAtom = atomWithLocation();
+export const locationAtom = atomWithLocation({ replace: true });
