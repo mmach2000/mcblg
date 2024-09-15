2d4a68d8d93031e32e46328934c7ab18e48aaf5d
glatavento <glatavento@outlook.com>
2024-09-15 13:27:32 +0800

    feat(content): 优化内容

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ export const collections = {
   docs: defineCollection({
     schema: docsSchema({
       extend: z.object({
+        author: z.string().optional(),
         tags: z.string().array().optional(),
         ctime: z.string().optional(),
         mtime: z.string().optional(),
